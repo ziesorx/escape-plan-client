93ebefa3d7dae1a7d1eeb73c1fdff3f5b24fe0c2
Sukirin Boonrod <sukirintlbcc@gmail.com>
2022-10-21 16:54:59 +0700

    waiting page update

--- pages/waiting.js
+++ pages/waiting.js
@@ -2,6 +2,7 @@ import Router from 'next/router';
 import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { avatars } from '../variables/avatars';
 
 import {
   Button,
@@ -16,13 +17,14 @@ import {
   ModalFooter,
 } from 'reactstrap';
 
-const waiting = () => {
+const Waiting = () => {
   const backToIndex = () => {
     Router.push('/');
   };
 
   const [modal, setModal] = useState(false);
   const toggle = () => setModal(!modal);
+  const isHost = () => {};
 
   useEffect(() => {
     console.log(FontAwesomeIcon);
@@ -38,10 +40,13 @@ const waiting = () => {
             // height: '90vh',
           }}
         >
-          <Row className="justify-content-between" style={{ gap: '5rem' }}>
+          <Row className="justify-content-center" style={{ gap: '5rem' }}>
             <Row>
               <div>
-                <Button className="mt-2 mx-2 btn-create" onClick={backToIndex}>
+                <Button
+                  className="mt-2 mx-2 btn-create position-absolute top-0 start-0"
+                  onClick={backToIndex}
+                >
                   Back
                 </Button>
                 <Row className="justify-content-center">
@@ -51,7 +56,6 @@ const waiting = () => {
                 </Row>
               </div>
             </Row>
-
             <Row className="justify-content-center">
               <Col md="4">
                 <Card className="text-md-center fs-2">
@@ -59,7 +63,7 @@ const waiting = () => {
                     <span>Player 1</span>
                   </Row>
                   <Row>
-                    <img src="/img/police-icon.png" />
+                    <img src={avatars[0].img_src} />
                   </Row>
                 </Card>
               </Col>
@@ -73,7 +77,7 @@ const waiting = () => {
                     <span></span>
                   </Row>
                   <Row>
-                    <img src="/img/prisoner-icon.png" />
+                    <img src={avatars[1].img_src} />
                   </Row>
                 </Card>
               </Col>
@@ -81,8 +85,17 @@ const waiting = () => {
 
             <Row>
               <div>
-                <Button color="success" className="mb-2 mx-2">
+                <Button
+                  color="success"
+                  className="mb-2 mx-2 position-absolute bottom-0 start-0"
+                >
                   <FontAwesomeIcon icon={faUser} />
+                </Button>
+                <Button
+                  color="danger"
+                  className="mb-2 position-absolute bottom-0 end-50"
+                >
+                  Start
                 </Button>
                 <Button
                   className="mb-2 mx-2 btn-create position-absolute bottom-0 end-0"
@@ -95,7 +108,7 @@ const waiting = () => {
                     How to play this game
                   </ModalHeader>
                   <ModalBody>
-                    One day, there is a Nigga who's a chad, namely Pond, and he
+                    One day, there is a man who's a chad, namely Pond, and he
                     stole the most prestiegeous diamond in the world from the
                     most famous shop keeper name Poraor. Try your best to escape
                     or catch the chad!!!
@@ -118,4 +131,4 @@ const waiting = () => {
   );
 };
 
-export default waiting;
+export default Waiting;
