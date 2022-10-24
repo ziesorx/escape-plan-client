af4e87e8a556f1251f33c34dc1f44924f66db562
Tanakit Thangajantaraprapab <87474610+Jackietanakit@users.noreply.github.com>
2022-10-24 15:45:55 +0700

    update test

--- pages/test/index.js
+++ pages/test/index.js
@@ -17,12 +17,32 @@ import { avatars } from '../../variables/avatars';
 
 const TestPage = () => {
   useEffect(() => {
-    socket.on('room:create-done', (roomId, userInfo) => {
-      console.log(roomId, userInfo);
+    socket.on('user:error', (message) => {
+      console.log(message);
     });
 
-    socket.on('room:join-done', (name, userInfo) => {
-      console.log(name, userInfo);
+    socket.on('room:error', (message) => {
+      console.log(message);
+    });
+
+    socket.on('user:login-done', (userInfo) => {
+      console.log(userInfo);
+    });
+
+    socket.on('user:info-done', (userInfo) => {
+      console.log(userInfo);
+    });
+
+    socket.on('user:get-all-done', (userInSocket) => {
+      console.log(userInSocket);
+    });
+
+    socket.on('room:create-done', (roomDetail) => {
+      console.log(roomDetail);
+    });
+
+    socket.on('room:join-done', (roomDetail) => {
+      console.log(roomDetail);
     });
 
     socket.on('room:start-done', (gameEl) => {
@@ -33,62 +53,78 @@ const TestPage = () => {
       console.log(userInfo);
     });
 
-    socket.on('room:delete-done', (socketRooms) => {
-      console.log(socketRooms);
-    });
+    // socket.on('room:delete-done', (socketRooms) => {
+    //   console.log(socketRooms);
+    // });
 
-    socket.on('room:current-done', (socketRooms) => {
-      console.log(socketRooms);
-    });
+    // socket.on('room:current-done', (socketRooms) => {
+    //   console.log(socketRooms);
+    // });
 
-    socket.on('game:coor-done', (coor, isWarder) => {
-      console.log(coor, isWarder);
-    });
+    // socket.on('game:coor-done', (coor, isWarder) => {
+    //   console.log(coor, isWarder);
+    // });
 
-    socket.on('game:again-done', (gameEl) => {
-      console.log(gameEl);
-    });
+    // socket.on('game:again-done', (gameEl) => {
+    //   console.log(gameEl);
+    // });
 
-    socket.on('user:info-done', (userInSocket) => {
-      console.log(userInSocket);
-    });
+    // socket.on('user:info-done', (userInSocket) => {
+    //   console.log(userInSocket);
+    // });
 
-    socket.on('user:score-done', (userInfo) => {
-      console.log(userInfo);
-    });
+    // socket.on('user:score-done', (userInfo) => {
+    //   console.log(userInfo);
+    // });
   }, []);
 
+  const onLogin1 = () => {
+    socket.emit('user:login', 'jackie', 1);
+  };
+
+  const onLogin2 = () => {
+    socket.emit('user:login', 'jimmy', 2);
+  };
+
   const onCreate = () => {
-    socket.emit('room:create', 'Jackie', 1);
+    socket.emit('room:create', 'jackie', 'jimmy');
   };
 
   const onJoin = () => {
-    socket.emit('room:join', 'Jimmy', 2, '111111');
+    socket.emit('room:join', '111111');
   };
 
   const onStart = () => {
-    socket.emit('room:start');
+    socket.emit('room:start', 'jackie', 'jimmy');
+  };
+
+  const onGetAllUser = () => {
+    socket.emit('user:get-all');
+  };
+
+  const onGetUserInfo = () => {
+    socket.emit('user:info');
   };
 
   const onLeave = () => {
-    socket.emit('user:leave');
+    socket.emit('room:leave');
   };
 
-  const onDisconnect = () => {
-    socket.emit('disconnect');
-  };
+  // const onDisconnect = () => {
+  //   socket.emit('disconnect');
+  // };
 
-  const onCoorUpdate = () => {
-    socket.emit('coor:update');
-  };
+  // const onCoorUpdate = () => {
+  //   socket.emit('coor:update');
+  // };
 
-  const onRoomDelete = () => {
-    socket.emit('room:delete');
-  };
+  // const onRoomDelete = () => {
+  //   socket.emit('room:delete');
+  // };
 
-  const onCurrentRoom = () => {
-    socket.emit('room:current');
-  };
+  // const onCurrentRoom = () => {
+  //   socket.emit('room:current');
+  // };
 
   return (
     <>
@@ -102,6 +138,30 @@ const TestPage = () => {
               }}
             >
               <CardBody className="px-lg-5 py-lg-5">
+                <Row>
+                  <Col>
+                    <Button
+                      block
+                      className="mt-4"
+                      onClick={onLogin1}
+                      size="lg"
+                      color="primary"
+                    >
+                      user:login1
+                    </Button>
+                  </Col>
+                  <Col>
+                    <Button
+                      block
+                      className="mt-4"
+                      onClick={onLogin2}
+                      size="lg"
+                      color="primary"
+                    >
+                      user:login2
+                    </Button>
+                  </Col>
+                </Row>
                 <Row>
                   <Col>
                     <Button
@@ -131,26 +191,50 @@ const TestPage = () => {
                     <Button
                       block
                       className="mt-4"
-                      onClick={onCurrentRoom}
+                      onClick={onStart}
                       size="lg"
                       color="primary"
                     >
-                      room:current
+                      room:start
                     </Button>
                   </Col>
                   <Col>
                     <Button
                       block
                       className="mt-4"
-                      onClick={onRoomDelete}
+                      onClick={onLeave}
                       size="lg"
                       color="primary"
                     >
-                      room:delete
+                      room:leave
                     </Button>
                   </Col>
                 </Row>
                 <Row>
+                  <Col>
+                    <Button
+                      block
+                      className="mt-4"
+                      onClick={onGetAllUser}
+                      size="lg"
+                      color="primary"
+                    >
+                      user:get-all
+                    </Button>
+                  </Col>
+                  <Col>
+                    <Button
+                      block
+                      className="mt-4"
+                      onClick={onGetUserInfo}
+                      size="lg"
+                      color="primary"
+                    >
+                      user:info
+                    </Button>
+                  </Col>
+                </Row>
+                {/* <Row>
                   <Col>
                     <Button
                       block
@@ -162,7 +246,7 @@ const TestPage = () => {
                       coor:update
                     </Button>
                   </Col>
-                  {/* <Col>
+                  <Col>
                     <Button
                       block
                       className="mt-4"
@@ -172,7 +256,7 @@ const TestPage = () => {
                     >
                       user:leave
                     </Button>
-                  </Col> */}
+                  </Col>
                   <Col>
                     <Button
                       block
@@ -197,7 +281,7 @@ const TestPage = () => {
                       disconnect
                     </Button>
                   </Col>
-                </Row>
+                </Row> */}
               </CardBody>
             </Card>
           </Col>
