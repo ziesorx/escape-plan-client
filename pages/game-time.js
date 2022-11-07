9dd9a331bf923cefe14fe2aabff1c2fbf8a54c5a
ziesorx <nwisessan@hotmail.com>
2022-11-07 19:10:39 +0700

    Update game page

--- pages/game-time.js
+++ pages/game-time.js
@@ -1,96 +1,107 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @next/next/no-img-element */
-import { WrappedBuildError } from 'next/dist/server/base-server'
-import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
-import { Card, Row, Col, Container } from 'reactstrap'
-import Swal from 'sweetalert2'
-import Header from '../components/Header'
-import { socket } from '../services/socket'
+import { WrappedBuildError } from 'next/dist/server/base-server';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Card, Row, Col, Container } from 'reactstrap';
+import Swal from 'sweetalert2';
+import Header from '../components/Header';
+import { socket } from '../services/socket';
 
 const GamePage = () => {
-  const { user } = useSelector((state) => state.user)
-  const { opponent } = useSelector((state) => state.user)
-  const { currentRoom } = useSelector((state) => state.room)
-  const [isHost, setIsHost] = useState(false)
-  const [isWarder, setIsWarder] = useState(false)
-  const [isWarderTurn, setIsWarderTurn] = useState(true)
-  const [isHover, setIsHover] = useState(false)
-  const [alreadyWalk, setAlreadyWalk] = useState(false)
-  const [matrix, setMatrix] = useState(null)
-  const [timer, setTimer] = useState(10)
-  const [goingCoor, setGoingCoor] = useState(null)
+  const { user } = useSelector(state => state.user);
+  const { opponent } = useSelector(state => state.user);
+  const { currentRoom } = useSelector(state => state.room);
+  const [isHost, setIsHost] = useState(false);
+  const [isWarder, setIsWarder] = useState(false);
+  const [isWarderTurn, setIsWarderTurn] = useState(true);
+  const [isHover, setIsHover] = useState(false);
+  const [alreadyWalk, setAlreadyWalk] = useState(false);
+  const [matrix, setMatrix] = useState(null);
+  const [timer, setTimer] = useState(10);
+  const [goingCoor, setGoingCoor] = useState(null);
 
   useEffect(() => {
-    user.name === currentRoom?.users.filter((user) => !!user.isHost)[0].name &&
-      setIsHost(true)
+    user.name === currentRoom?.users.filter(user => !!user.isHost)[0].name &&
+      setIsHost(true);
 
-    socket.on('room:start-done', (gameElement) => {
-      console.log(gameElement)
-      setMatrix(gameElement.mapDetail.map)
+    socket.on('room:start-done', gameElement => {
+      console.log(gameElement);
+      setMatrix(gameElement.mapDetail.map);
       const myUser = gameElement.users.filter(
-        (currentUser) => currentUser.name === user.name
-      )[0]
-      setIsWarder(myUser.isWarder)
-    })
+        currentUser => currentUser.name === user.name
+      )[0];
+      setIsWarder(myUser.isWarder);
+    });
 
-    socket.on('game:update-done', (newCoor) => {
-      console.log(newCoor)
-      setMatrix(newCoor.mapDetail.map)
-      setAlreadyWalk(false)
-      setIsWarderTurn(newCoor.isWarderTurn)
-    })
+    socket.on('game:update-done', newCoor => {
+      console.log(newCoor);
+      setMatrix(newCoor.mapDetail.map);
+      setAlreadyWalk(false);
+      setIsWarderTurn(newCoor.isWarderTurn);
+    });
 
-    socket.on('game:end-done', (winnerUserInfo) => {
+    socket.on('game:end-done', winnerUserInfo => {
+      setTimer(-1);
+      const winUser = gameElement.users.filter(
+        user => user.name === winnerUserInfo.name
+      )[0];
+
       Swal.fire({
-        title: `${winnerUserInfo.isWarder ? 'Warder' : 'Prisoner'} is win!`,
+        title: `${winUser.isWarder ? 'Warder' : 'Prisoner'} is win!`,
         text: `Congratz ${winnerUserInfo.name}!!`,
         showDenyButton: true,
         confirmButtonText: 'Play again?',
         denyButtonText: `Leave room`,
-      }).then((result) => {
+      }).then(result => {
         if (result.isConfirmed) {
-          Swal.fire('kuay wai gorn')
+          Swal.fire('kuay wai gorn');
         } else if (result.isDenied) {
-          Swal.fire('ook pai')
+          Swal.fire('ook pai');
         }
-      })
-    })
-  }, [])
+      });
+    });
+  }, []);
 
   useEffect(() => {
-    if (isHost) socket.emit('room:start', user.name, opponent.name)
-  }, [isHost])
+    if (isHost) socket.emit('room:start', user.name, opponent.name);
+  }, [isHost]);
 
   useEffect(() => {
-    timer > 0 && setTimeout(() => setTimer((prev) => prev - 1), 1000)
+    if (timer === -1) return;
+
+    let timeout;
+
+    if (timer > 0) timeout = setTimeout(() => setTimer(prev => prev - 1), 1000);
 
     if (timer === 0) {
       if (isWarder === isWarderTurn) {
-        console.log(goingCoor)
-        socket.emit('game:update', goingCoor, isWarderTurn)
+        console.log(goingCoor);
+        socket.emit('game:update', goingCoor, isWarderTurn);
       }
     }
-  }, [timer])
+
+    return () => clearTimeout(timeout);
+  }, [timer]);
 
   useEffect(() => {
-    setTimer(10)
-  }, [isWarderTurn])
+    setTimer(10);
+  }, [isWarderTurn]);
 
   const findPos = (array, symbol) => {
-    const string = array.toString().replace(/,/g, '')
-    const pos = string.indexOf(symbol)
+    const string = array.toString().replace(/,/g, '');
+    const pos = string.indexOf(symbol);
 
-    const d = (array[0] || []).length
+    const d = (array[0] || []).length;
 
-    const x = pos % d
-    const y = Math.floor(pos / d)
+    const x = pos % d;
+    const y = Math.floor(pos / d);
 
-    return { y, x }
-  }
+    return { y, x };
+  };
 
-  const highlightTile = (coord) => {
-    const charCoor = isWarder ? findPos(matrix, 'w') : findPos(matrix, 'p')
+  const highlightTile = coord => {
+    const charCoor = isWarder ? findPos(matrix, 'w') : findPos(matrix, 'p');
 
     if (isWarder === isWarderTurn && !alreadyWalk && isHover) {
       if (
@@ -105,22 +116,22 @@ const GamePage = () => {
           coord.y === findPos(matrix, 'w').y &&
           coord.x === findPos(matrix, 'w').x
         )
-          return 'danger-highlighted'
+          return 'danger-highlighted';
         else if (
           isWarder &&
           coord.y === findPos(matrix, 'p').y &&
           coord.x === findPos(matrix, 'p').x
         )
-          return 'success-highlighted'
+          return 'success-highlighted';
 
-        return 'highlighted'
+        return 'highlighted';
       } else {
-        return ''
+        return '';
       }
     }
-  }
+  };
 
-  const renderCharacter = (character) => {
+  const renderCharacter = character => {
     if (character === 'w') {
       return (
         <>
@@ -133,7 +144,7 @@ const GamePage = () => {
             onMouseLeave={() => setIsHover(false)}
           />
         </>
-      )
+      );
     } else if (character === 'p') {
       return (
         <>
@@ -146,7 +157,7 @@ const GamePage = () => {
             onMouseLeave={() => setIsHover(false)}
           />
         </>
-      )
+      );
     } else if (character === 1) {
       return (
         <>
@@ -157,7 +168,7 @@ const GamePage = () => {
             style={{ height: '100%', objectFit: 'cover' }}
           />
         </>
-      )
+      );
     } else if (character === 'h') {
       return (
         <>
@@ -168,27 +179,27 @@ const GamePage = () => {
             style={{ height: '100%', objectFit: 'cover' }}
           />
         </>
-      )
+      );
     }
 
-    return <div className="free-space-tile"></div>
-  }
+    return <div className="free-space-tile"></div>;
+  };
 
   const updateCurrentBoard = (rowIdx, columnIdx) => {
-    const charCoor = isWarder ? findPos(matrix, 'w') : findPos(matrix, 'p')
+    const charCoor = isWarder ? findPos(matrix, 'w') : findPos(matrix, 'p');
 
     if (
       !isWarder &&
       rowIdx === findPos(matrix, 'w').y &&
       columnIdx === findPos(matrix, 'w').x
     ) {
-      return
+      return;
     } else if (
       isWarder &&
       rowIdx === findPos(matrix, 'h').y &&
       columnIdx === findPos(matrix, 'h').x
     ) {
-      return
+      return;
     }
 
     if (
@@ -198,38 +209,38 @@ const GamePage = () => {
         !(rowIdx === charCoor.y && columnIdx === charCoor.x + 1) &&
         !(rowIdx === charCoor.y && columnIdx === charCoor.x - 1))
     )
-      return
+      return;
 
-    setGoingCoor([rowIdx, columnIdx])
-    setAlreadyWalk(true)
+    setGoingCoor([rowIdx, columnIdx]);
+    setAlreadyWalk(true);
 
-    setMatrix((prevBoard) => {
-      const newBoard = [...prevBoard]
+    setMatrix(prevBoard => {
+      const newBoard = [...prevBoard];
 
       if (
         isWarder &&
         rowIdx === findPos(matrix, 'p').y &&
         columnIdx === findPos(matrix, 'p').x
       ) {
-        socket.emit('game:end')
+        socket.emit('game:end');
       } else if (
         !isWarder &&
         rowIdx === findPos(matrix, 'h').y &&
         columnIdx === findPos(matrix, 'h').x
       ) {
-        socket.emit('game:end')
-        return
+        socket.emit('game:end');
+        return;
       }
 
-      newBoard[charCoor.y] = [...newBoard[charCoor.y]]
-      newBoard[charCoor.y][charCoor.x] = 0
-      newBoard[rowIdx][columnIdx] = isWarder ? 'w' : 'p'
+      newBoard[charCoor.y] = [...newBoard[charCoor.y]];
+      newBoard[charCoor.y][charCoor.x] = 0;
+      newBoard[rowIdx][columnIdx] = isWarder ? 'w' : 'p';
 
-      return newBoard
-    })
-  }
+      return newBoard;
+    });
+  };
 
-  if (!matrix) return
+  if (!matrix) return;
 
   return (
     <Container className="mt--6" fluid>
@@ -273,16 +284,16 @@ const GamePage = () => {
                             )}`}
                             onClick={() => {
                               if (isWarder === isWarderTurn && !alreadyWalk) {
-                                updateCurrentBoard(rowIdx, columnIdx)
+                                updateCurrentBoard(rowIdx, columnIdx);
                               }
                             }}
                           >
                             {renderCharacter(column)}
                           </Col>
-                        )
+                        );
                       })}
                     </Row>
-                  )
+                  );
                 })}
               </Col>
             </Row>
@@ -290,7 +301,7 @@ const GamePage = () => {
         </Col>
       </Row>
     </Container>
-  )
-}
+  );
+};
 
-export default GamePage
+export default GamePage;
