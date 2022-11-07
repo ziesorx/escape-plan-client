1e1fd06a809ef3773823e08627e9d25ef1fde5b4
Dolpatra D <d.dolpattra@gmail.com>
2022-11-07 18:22:36 +0700

    Update game page

--- services/socket.js
+++ services/socket.js
@@ -1,6 +1,6 @@
-import { io } from 'socket.io-client';
+import { io } from 'socket.io-client'
 
 // export const socket = io('http://localhost:8000');
 
-// export const socket = io('https://mysterious-scrubland-34928.herokuapp.com/')
-export const socket = io('http://192.168.1.57:8000/');
+export const socket = io('https://mysterious-scrubland-34928.herokuapp.com/')
+// export const socket = io('http://192.168.1.57:8000/');
