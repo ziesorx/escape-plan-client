e8676537e40f71e5474648918d146c7751e2f01f
ziesorx <nwisessan@hotmail.com>
2022-11-25 01:12:47 +0700

    Change flex to grid

--- layouts/Layout.js
+++ layouts/Layout.js
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import Router, { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 const Layout = ({ children }) => {
@@ -22,10 +22,12 @@ const Layout = ({ children }) => {
 
   return (
     <div
-      className="w-100 vh-100 d-inline-flex flex-column justify-content-center"
+      className="w-100 vh-100"
       style={{
         backgroundImage: 'url("/img/city-bg.gif")',
         backgroundSize: 'cover',
+        display: 'grid',
+        placeItems: 'center',
       }}
     >
       {children}
