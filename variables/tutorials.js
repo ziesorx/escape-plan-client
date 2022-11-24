81edd78f9b764f116c6238ea4f2610d1a66412d5
ziesorx <nwisessan@hotmail.com>
2022-11-25 00:33:12 +0700

    Update tutorial texts

--- variables/tutorials.js
+++ variables/tutorials.js
@@ -3,7 +3,7 @@ export const tutorials = [
     id: 1,
     src: '/tutorial-gif/movement.gif',
     header: 'How to move?',
-    text: 'Players can move to the adjacent tile by clicking the tile. The indicator showing the available tile will apear upon hovering over the character',
+    text: 'Players can move to the adjacent tile by clicking the tile. The indicator showing the movable tile will appear upon hovering on the character',
   },
   {
     id: 2,
@@ -15,6 +15,6 @@ export const tutorials = [
     id: 3,
     src: '/tutorial-gif/win-condition.gif',
     header: 'How to win?',
-    text: 'Win conidtion for warder is to catch the theif while the win condition for theif is to escape through the green tunnel. The message will appear upon winning or losing and the game will end',
+    text: 'Win conidtion for warder is to catch the prisoner while the win condition for prisoner is to escape through the green tunnel.',
   },
 ];
